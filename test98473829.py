c5696ecb2513302b29e3991ca77dcc6b2a6795b9
thisissultan <sultan.tj.kgz@gmail.com>
2023-01-21 19:07:18 +0600

    first test

--- test98473829.py
+++ test98473829.py
@@ -1,3 +1,20 @@
-#wertykjhgf
+size(600,400)
+print(("ваш случайный номер "),int(random(550000000, 559999999)))
 
-#werfds
+def fun():
+    fill(int(random(0, 255)),int(random(0, 255)),int(random(0, 255)))
+    rect(60+100*a,10+50*b,40,40)
+    circle(30+100*a,30+50*b,40)
+    
+c=(int(random(0, 2)))
+print(c)
+
+for a in range(6):
+    for b in range(5):
+        
+        if c==1:
+          fill(int(random(0, 255)),int(random(0, 255)),int(random(0, 255)))
+          rect(10+100*a,10+50*b,40,40)
+          circle(80+100*a,30+50*b,40)
+        else:
+          fun()
